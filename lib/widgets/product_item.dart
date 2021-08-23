b39a8f7838326dea0c64069f565df2fab6c61755
Joana Freitas <joanafreitassalles@hotmail.com>
2021-08-22 23:03:41 -0300

    inicio ultilização provider

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shop/views/product_detail_screen.dart';
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => ProductDetailScreen(product),
+              ),
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
